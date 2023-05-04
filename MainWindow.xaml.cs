acdabb05842422a5addf4664cfcce445c4ab2937
Wiktor <kremobil@wp.pl>
2023-05-05 00:52:33 +0200

    finished labels

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Navigation;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Navigation;
 
 namespace WpfApplications_tutorial
 {
@@ -10,6 +14,20 @@ namespace WpfApplications_tutorial
         public MainWindow()
         {
             InitializeComponent();
+
+            //---------- Adding a text block from code side ----------
+            // TextBlock textBlockTwo = new TextBlock();
+            // textBlockTwo.Text = "Text block from c#";
+            // textBlockTwo.Inlines.Add("This is added using inlines");
+            // textBlockTwo.Inlines.Add(new Run(" Run text that I added in code side")
+            // {
+            //     Foreground = Brushes.Chartreuse,
+            //     TextDecorations = TextDecorations.Underline
+            // });
+            // this.Content = textBlockTwo;
+
+            TextBlockOne.Text = "Text from C# side";
+            TextBlockOne.Foreground = Brushes.Blue;
             myButton.FontSize = 48;
             myButton.Content = "Hello Wiktor";
         }
@@ -18,5 +36,6 @@ namespace WpfApplications_tutorial
         {
             System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
         }
+        
     }
 }
