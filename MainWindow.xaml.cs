74a0a3da88e153fd6c6c6b51addd77d9939b7ec1
Wiktor <kremobil@wp.pl>
2023-05-04 22:39:57 +0200

    Started C# WPF tutorial currently at TextBlock stage

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Navigation;
+
+namespace WpfApplications_tutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            myButton.FontSize = 48;
+            myButton.Content = "Hello Wiktor";
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+    }
+}
